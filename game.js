c7dd26b1b50eaf34250f6a7e1ba1449117f60488
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-09-12 20:17:22 +0530

    Getting questions

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestion = [];
 
-let question = [
+let questions = [
     {
         question : "Which one is correct team name in NBA?",
         options : [
@@ -49,3 +49,30 @@ let question = [
         answer: "Huston Rocket"
     }
 ]
+
+const bonus = 1;
+const maxQuestion = 4;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0 ;
+    availableQuestion = [...questions];
+    getQuestion();
+}
+
+getQuestion=() => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestion.length);
+    ;
+    currentQuestion = availableQuestion[questionIndex];
+    question.innerText = currentQuestion.question;
+    console.log(currentQuestion)
+
+    choices.forEach( choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion.options[choice];
+        console.log(choice.innerText);
+    })
+}
+
+startGame();
